ee31af0366bb037faf56d425b217dbc3c8fed341
Kevin Salina <kevin.salina25@gmail.com>
2021-06-29 20:10:05 -0400

    testing out TDD with Mocha and Chai

---
+++ tests/calculation.test.js
@@ -0,0 +1,14 @@
+const describe = require('mocha').describe
+const it = require('mocha').it
+
+describe('calculation', () => {
+  describe('sumALL', () => {
+    it('return the sum of the numbers in the array provided', () => {
+      // data to be tested
+
+      // call the function with the test data
+
+      // assets what should be true
+    })
+  })
+})
